d1286d4991076fd7f30b69706ada879b548aabd5
Victor Hogeweij <hogeweyv@gmail.com>
2023-09-11 18:09:34 +0200

    -enh improved folder structure

--- pio_build_configure.py
+++ pio_build_configure.py
@@ -8,12 +8,11 @@ hal_var_found = False
 # Private flags (only for the current "SomeLib" source files)
 #
 platform = env.get("PIOPLATFORM")
-path = join(os.getcwd()+"/hal_gpio/platform/", platform)
+path = join(os.getcwd()+"/hal/platform/", platform)
 if(os.path.exists(path)):
     hal_var_found = True
-    env.Append(CPPPATH=[realpath("hal_gpio/"), realpath(join("hal_gpio/platform/", platform))])
-    env.Append(CPPPATH=[realpath("hal_i2c"), realpath(join("hal_i2c/platform/", platform))])
-    env.Replace(SRC_FILTER=["+<hal_gpio/gpio.h>","+<hal_gpio/gpio_error_handling.h>", "+<*/platform/%s/*>" % platform])
+    env.Append(CPPPATH=[realpath("hal"), realpath(join("hal/platform/", platform))])
+    env.Replace(SRC_FILTER=["+<hal/*>", "+<hal/platform/%s/*>" % platform])
 else:
     print("ERROR! MCU: %s specified in pio config is not supported" % platform)
     sys.exit()
