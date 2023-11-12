9ba4e8ca5413295a828fe6500fbe15f465192fdc
Victor Hogeweij <vo.hogeweij@student.han.nl>
2023-11-12 14:15:29 +0100

    -add license banners to all source and header files

--- hal/platform/atmelsam/i2c_host/i2c_host_irq_handler.h
+++ hal/platform/atmelsam/i2c_host/i2c_host_irq_handler.h
@@ -1,3 +1,31 @@
+/**
+* \file            i2c_host_irq_handler.h
+* \brief           Header file which implements the default I2C HOST IRQ Handler
+*/
+/*
+*  Copyright 2023 (C) Victor Hogeweij <hogeweyv@gmail.com>
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+* This file is part of the Universal Hal Framework.
+*
+* This file contains contributions made by members of the Health Concept Lab.
+* For more info about the Health Concept Lab see:
+* https://www.han.nl/opleidingen/hbo/embedded-systems-engineering/voltijd/maak-kennis/HCL-introductiepresentatie-V2.pdf
+*
+* Authors:          Victor Hogeweij <hogeweyv@gmail.com>
+*                   Johan Korten <Johan.Korten@han.nl>
+*/
 #ifndef I2C_HOST_IRQ_HANDLER_H
 #define I2C_HOST_IRQ_HANDLER_H
 #include <stdbool.h>
--- hal/platform/atmelsam/irq/irq_bindings.c
+++ hal/platform/atmelsam/irq/irq_bindings.c
@@ -1,3 +1,28 @@
+/**
+* \file            irq_bindings.c
+* \brief           Source file which binds peripheral/system irq handlers 
+*                  to the internal module irq handlers of this framework.
+*/
+/*
+*  Copyright 2023 (C) Victor Hogeweij <hogeweyv@gmail.com>
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+* This file is part of the Universal Hal Framework.
+*
+* Author:          Victor Hogeweij <hogeweyv@gmail.com>
+*/
+
 #include <stddef.h>
 #include <sam.h>
 #include <irq/sercom_stuff.h>
@@ -54,7 +79,7 @@ static inline void default_sercom_isr_handler(const void* const hw, volatile bus
             break;
         }
         #endif
-        
+
         #ifndef DISABLE_I2C_HOST_MODULE
         case SERCOMACT_I2C_DATA_TRANSMIT_NO_STOP:
         case SERCOMACT_I2C_DATA_TRANSMIT_STOP: {
