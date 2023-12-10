b34193ec35516a85a96902ce2c26cc1251c0ef70
Victor Hogeweij <vo.hogeweij@student.han.nl>
2023-12-10 16:41:20 +0100

    -add some test cases for the gpio module

--- test/atmelsam/mocks/sam.h
+++ test/atmelsam/mocks/sam.h
@@ -1,18 +1,12 @@
 #ifndef SAM_H
 #define SAM_H
-/* Extern c for compiling with c++*/
-#ifdef __cplusplus
-extern "C" {
-#endif /* __cplusplus */
+
+#define PORT_GROUPS 2
 
 #include "port_mock.h"
 #include "eic_mock.h"
 #include "sercom_mock.h"
 #include "gclk_mock.h"
 #include "nvic_mock.h"
-
-#ifdef __cplusplus
-}
-#endif /* __cplusplus */
 
 #endif
