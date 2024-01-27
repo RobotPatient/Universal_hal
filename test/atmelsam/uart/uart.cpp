fb43ad89e3f5d649cba63078623a5e76c6929cab
Victor Hogeweij <vo.hogeweij@student.han.nl>
2024-01-27 16:41:51 +0100

    -add more test conditions

--- test/atmelsam/uart/uart.cpp
+++ test/atmelsam/uart/uart.cpp
@@ -56,17 +56,47 @@ TEST(HAL_UART, BAUD_CALCULATION_OVSMPL_16_ARITH) {
             baudrate = common_baud_rates[i];
             clock_source_freq = (48e6);
             clock_source_div = (clock_source_freq);
-            expected_baud = 65536 - ((65536 * 16.0f * (baudrate)) / (clock_source_div));
+            expected_baud = 65536 - ((65535 * 16.0f * (baudrate)) / (clock_source_div));
 
             uart_init((uart_peripheral_inst_t) peripheral, baudrate, UART_CLK_SOURCE_USE_DEFAULT, clock_source_freq, UART_EXTRA_OPT_USE_DEFAULT);
 
-            EXPECT_EQ(MockSercom[peripheral].USART.BAUD.reg, expected_baud) << "Testing baudrate: " << std::to_string(baudrate) << '\n';
+            EXPECT_EQ(MockSercom[peripheral].USART.BAUD.reg, expected_baud) << "Testing baudrate: " 
+                                                                            << std::to_string(baudrate) 
+                                                                            << " With peripheral num: " 
+                                                                            << std::to_string(peripheral)
+                                                                            << '\n';
        }
     }
 }
 
 TEST(HAL_UART, BAUD_CALCULATION_OVSMPL_16_FRACT) {
     memset(MockSercom, 0x00, sizeof(MockSercom));
+    uint32_t baudrate, clock_source_div, clock_source_freq;
+    uint64_t baudmult, expected_baud, expected_baud_fp;
+
+    for (uint8_t peripheral = UART_PERIPHERAL_0; peripheral < UART_PERIPHERAL_5; peripheral++) {
+        for (uint32_t i = 0; i < (sizeof(common_baud_rates)/sizeof(uint32_t)); i++) {
+            baudrate = common_baud_rates[i];
+            clock_source_freq = (48e6);
+            clock_source_div = (clock_source_freq);
+            baudmult = (clock_source_freq * 8) / (16 * baudrate);
+            //expected_baud = 65536 - ((65536 * 16.0f * (baudrate)) / (clock_source_div));
+            expected_baud = (baudmult / 8);
+            expected_baud_fp = (baudmult % 8);
+            uart_init((uart_peripheral_inst_t) peripheral, baudrate, UART_CLK_SOURCE_USE_DEFAULT, clock_source_freq, UART_EXTRA_OPT_OVERSAMPL_16X_FRACT);
+
+            EXPECT_EQ(MockSercom[peripheral].USART.BAUD.FRAC.BAUD, expected_baud) << "Testing baudrate: " 
+                                                                            << std::to_string(baudrate) 
+                                                                            << " With peripheral num: " 
+                                                                            << std::to_string(peripheral)
+                                                                            << '\n';
+            EXPECT_EQ(MockSercom[peripheral].USART.BAUD.FRAC.FP, expected_baud) << "Testing baudrate: " 
+                                                                            << std::to_string(baudrate) 
+                                                                            << " With peripheral num: " 
+                                                                            << std::to_string(peripheral)
+                                                                            << '\n';
+       }
+    }
 }
 
 
